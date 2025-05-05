2f9bc8b7a08f20d986f7f53ad2a0ec45afd2e3f2
TechAle <alessandro.condello.email@gmail.com>
2025-05-05 16:15:47 +0200

    Test

---
+++ scraper/Scraper.py
@@ -0,0 +1,8 @@
+class scraper:
+    def __init__(self, configurationPath):
+        self.url = url
+
+    def scrape(self):
+        # Simulate scraping the URL
+        print(f"Scraping data from {self.url}")
+        return {"data": "sample data"}
