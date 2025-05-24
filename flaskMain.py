e7f55431aaead17aa53fa64d17d5debccb530ad2
TechAle <alessandro.condello.email@gmail.com>
2025-05-24 10:25:24 +0200

    Changed the architecture idea: now i'm using react-native instead of a website

--- flaskMain.py
+++ flaskMain.py
@@ -1,28 +1,20 @@
 import os
 import threading
 import time
-
-from flask import Flask, jsonify, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-from webserver.utils.ClassUtils import getClassesWithRules
 from importlib import import_module
 
-from webserver.webpush_handler import trigger_push_notifications_for_subscriptions
+from flask import Flask, render_template
+
+from webserver.utils.ClassUtils import getClassesWithRules
 
 
 class MyFlaskApp:
     def __init__(self):
         self.app = Flask(__name__, instance_relative_config=True)
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
-        self.app.config.from_pyfile('application.cfg.py')
 
-        self.db = SQLAlchemy(self.app)
-        self.scheduled_functions = []
-        self.message_log = []
         self.managers_functions = {}
         self.scheduled_functions = []
 
-        self.init_db_models()
         self.configure_routes()
 
         # Scheduler logic to avoid re-entrance in debug autoreload
@@ -32,16 +24,6 @@ class MyFlaskApp:
             self.start_scheduler()
         else:
             os.environ['RUNNING'] = "True"
-
-    def init_db_models(self):
-        class PushSubscription(self.db.Model):
-            id = self.db.Column(self.db.Integer, primary_key=True, unique=True)
-            subscription_json = self.db.Column(self.db.Text, nullable=False)
-
-        self.PushSubscription = PushSubscription
-        with self.app.app_context():
-            self.db.create_all()
-
     # Decorator to mark scheduled functions
     def scheduled_task(self, interval):
         def decorator(func):
@@ -73,9 +55,6 @@ class MyFlaskApp:
         # HTML routes
         self._add_html_routes()
 
-        # Push subscription API
-        self._add_push_subscription_routes()
-
         # Extra test/debug route
         self._add_extra_routes()
 
@@ -92,42 +71,11 @@ class MyFlaskApp:
         def admin_page():
             return render_template("webserver/templates/admin.html")
 
-    def _add_push_subscription_routes(self):
-        @self.app.route("/api/push-subscriptions", methods=["POST"])
-        def create_push_subscription():
-            json_data = request.get_json()
-            subscription = self.PushSubscription.query.filter_by(
-                subscription_json=json_data['subscription_json']
-            ).first()
-
-            if subscription is None:
-                subscription = self.PushSubscription(subscription_json=json_data['subscription_json'])
-                self.db.session.add(subscription)
-                self.db.session.commit()
-
-            return jsonify({
-                "status": "success",
-                "result": {
-                    "id": subscription.id,
-                    "subscription_json": subscription.subscription_json
-                }
-            })
-
-        @self.app.route("/admin-api/trigger-push-notifications", methods=["POST"])
-        def trigger_push_notifications():
-            json_data = request.get_json()
-            subscriptions = self.PushSubscription.query.all()
-            results = trigger_push_notifications_for_subscriptions(
-                subscriptions,
-                json_data.get('title'),
-                json_data.get('body')
-            )
-            return jsonify({"status": "success", "result": results})
 
     def _add_extra_routes(self):
         @self.app.route('/info')
         def info():
-            return f"Hello! {len(self.message_log)} messages received so far."
+            return f"Hello!."
 
     def run(self, **kwargs):
         self.app.run(**kwargs)
